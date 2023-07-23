cd7b7e787b65dc22bc46ce05efed8d898a3508c4
Chris Macharia <Christophermaina003@gmail.com>
2023-07-24 01:27:17 +0300

    Adjust the program to only use  print function with string format

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,12 @@
+def add(a, b):
+    return a + b
+
 if __name__ == "__main__":
     a = 1
     b = 2
 
-    # Import the add function from add_0.py
-    from add_0 import add
-
-    # Calculate the result using the add function
     result = add(a, b)
 
-    # Display the result using string formatting
+    # Display the result using a single print statement
     print(f"{a} + {b} = {result}")
 
