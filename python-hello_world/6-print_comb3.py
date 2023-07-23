c175ba090850ce03e1eff5f44dacfe1a9dd428f9
Chris Macharia <Christophermaina003@gmail.com>
2023-07-23 23:33:02 +0300

    Print out possible combinations of 2 numbers correctly

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
-for i in range(10):
+for i in range(1, 10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i), "{:02d}".format(j), sep=", ", end=", " if i < 8 or j < 9 else "\n")
+        print("{:02d}, {:02d}".format(i, j), end=", " if (i * 10 + j) < 89 else "\n")
 
