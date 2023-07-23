99e39f94397ceb2f98890299db9477123ac0dd99
Chris Macharia <Christophermaina003@gmail.com>
2023-07-23 23:41:41 +0300

    Fix: fix 6-print_comb3 to display correctly

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,8 @@
-for i in range(1, 10):
-    for j in range(i + 1, 10):
-        print("{:02d}, {:02d}".format(i, j), end=", " if (i * 10 + j) < 89 else "\n")
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        if i == 0:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:02d}".format(int(str(i)+str(j))), end=", ")
+print()
 
