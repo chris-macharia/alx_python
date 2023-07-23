02dfe0103dc2dd4b77b3d9fe00a6cae9e84cd72c
Chris Macharia <Christophermaina003@gmail.com>
2023-07-23 23:10:18 +0300

    Fix code to handle negative numbers better

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+numberOriginal = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+if (numberOriginal < 0):
+	number = abs(numberOriginal)
+else:
+	number = numberOriginal
 
-print("Last digit of {} is {}".format(number, last_digit), end=" ")
+last_digit = number % 10
+
+print("Last digit of {} is {}".format(numberOriginal, last_digit), end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
