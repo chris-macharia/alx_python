4aa1e14fa6cc17bec2752da757c4f1d039932d59
Chris Macharia <Christophermaina003@gmail.com>
2023-08-09 22:16:12 +0300

    Above all, Inherit the skills to do whatever you want on a computer

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function to check if object is exactly an instance of a specified class.
+
+Function:
+    is_same_class(obj, a_class): Checks if object is exactly an instance of the specified class.
+
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class representing our base geometry.
+
+    It can be used as a base class to create other geometry-related classes.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+
+    __doc__ = "This is an empty class representing our base geometry."
+    
+    pass
